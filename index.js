2cbb56c23e1ba64fe29075c4b432f3a8e79db0f4
Oscar Lastra <oscar.glastra@gmail.com>
2021-03-26 20:17:20 -0600

    Implement addTodo function

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ document.addEventListener('DOMContentLoaded', function (){
     const alert = document.getElementById('alert')
     const btn = document.getElementById('add');
 
-    btn.onclick = addTodo;
-
     function addTodo(){
         if(title.value === '' || description.value === ''){
             alert.classList.remove('d-none');
@@ -14,5 +12,22 @@ document.addEventListener('DOMContentLoaded', function (){
             return;
         }
         alert.classList.add('d-none');
+        const row = table.insertRow();
+        row.innerHTML = `
+            <td>${title.value}</td>
+            <td>${description.value}</td>
+            <td class="text-center">
+                <input type="checkbox">
+            </td>
+            <td class="text-right">
+                <button class="btn btn-primary mb-1">
+                  <i class="fa fa-pencil"></i>
+                </button>
+                <button class="btn btn-danger mb-1 ml-1">
+                  <i class="fa fa-trash"></i>
+                </button>
+            </td>
+        `;
     }
-});
+    btn.onclick = addTodo;
+}); 
