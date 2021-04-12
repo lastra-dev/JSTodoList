5956265f7a19c9a970e3ecb39b7d3113a5a677b5
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-04-12 17:26:26 -0500

    Change to MVC

---
+++ js/components/add-todo.js
@@ -0,0 +1,17 @@
+export default class AddTodo {
+  constructor() {
+    this.btn = document.getElementById("add");
+    this.title = document.getElementById("title");
+    this.description = document.getElementById("description");
+  }
+
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (title.value === "" || description.value === "") {
+        console.error("Incorrect");
+      } else {
+        callback(this.title.value, this.description.value);
+      }
+    };
+  }
+}
